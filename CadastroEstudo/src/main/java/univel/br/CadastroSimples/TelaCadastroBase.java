1a12622216ab94a4a67a46e60a3a809fe527e85b
Andre Tosetto <Andre Tosetto@Andre-PC.home>
2017-06-12 11:55:57 -0300

    Menu Base Criado

--- CadastroEstudo/src/main/java/univel/br/CadastroSimples/TelaCadastroBase.java
+++ CadastroEstudo/src/main/java/univel/br/CadastroSimples/TelaCadastroBase.java
@@ -6,10 +6,33 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class TelaCadastroBase extends JFrame {
 
 	private JPanel contentPane;
+	protected JTable table;
+	protected JTextField txfId;
+	protected JTextField txfNome;
+	protected JTextField txfTelefone;
+	protected JButton btnNovo;
+	protected JButton btnSalvar;
+	protected JButton btnExcluir;
+	protected JLabel lblCarregadoParaAtualizar;
+	private JMenu mnArquivo;
+	protected JMenuItem mntmImprimir;
+	protected JMenuItem mntmExportarpdf;
 
 	/**
 	 * Launch the application.
@@ -33,11 +56,151 @@ public class TelaCadastroBase extends JFrame {
 	public TelaCadastroBase() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnArquivo = new JMenu("Arquivo");
+		menuBar.add(mnArquivo);
+		
+		mntmImprimir = new JMenuItem("Imprimir");
+		mnArquivo.add(mntmImprimir);
+		
+		mntmExportarpdf = new JMenuItem("ExportarPDF");
+		mnArquivo.add(mntmExportarpdf);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		System.out.println("AAAAAAAAA");
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		contentPane.add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblId = new JLabel("Id");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.anchor = GridBagConstraints.WEST;
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 0;
+		panel.add(lblId, gbc_lblId);
+		
+		txfId = new JTextField();
+		txfId.setMinimumSize(new Dimension(1, 20));
+		txfId.setPreferredSize(new Dimension(1, 20));
+		GridBagConstraints gbc_txfId = new GridBagConstraints();
+		gbc_txfId.anchor = GridBagConstraints.WEST;
+		gbc_txfId.insets = new Insets(0, 0, 5, 5);
+		gbc_txfId.gridx = 1;
+		gbc_txfId.gridy = 0;
+		panel.add(txfId, gbc_txfId);
+		txfId.setColumns(5);
+		
+		lblCarregadoParaAtualizar = new JLabel("Carregado para atualizar");
+		GridBagConstraints gbc_lblCarregadoParaAtualizar = new GridBagConstraints();
+		gbc_lblCarregadoParaAtualizar.anchor = GridBagConstraints.WEST;
+		gbc_lblCarregadoParaAtualizar.insets = new Insets(0, 0, 5, 0);
+		gbc_lblCarregadoParaAtualizar.gridx = 2;
+		gbc_lblCarregadoParaAtualizar.gridy = 0;
+		panel.add(lblCarregadoParaAtualizar, gbc_lblCarregadoParaAtualizar);
+		
+		JLabel lblNome = new JLabel("Nome");
+		GridBagConstraints gbc_lblNome = new GridBagConstraints();
+		gbc_lblNome.anchor = GridBagConstraints.WEST;
+		gbc_lblNome.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNome.gridx = 0;
+		gbc_lblNome.gridy = 1;
+		panel.add(lblNome, gbc_lblNome);
+		
+		txfNome = new JTextField();
+		GridBagConstraints gbc_txfNome = new GridBagConstraints();
+		gbc_txfNome.gridwidth = 2;
+		gbc_txfNome.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txfNome.insets = new Insets(0, 0, 5, 5);
+		gbc_txfNome.gridx = 1;
+		gbc_txfNome.gridy = 1;
+		panel.add(txfNome, gbc_txfNome);
+		txfNome.setColumns(10);
+		
+		JLabel lblTelefone = new JLabel("Telefone");
+		GridBagConstraints gbc_lblTelefone = new GridBagConstraints();
+		gbc_lblTelefone.anchor = GridBagConstraints.EAST;
+		gbc_lblTelefone.insets = new Insets(0, 0, 0, 5);
+		gbc_lblTelefone.gridx = 0;
+		gbc_lblTelefone.gridy = 2;
+		panel.add(lblTelefone, gbc_lblTelefone);
+		
+		txfTelefone = new JTextField();
+		txfTelefone.setMinimumSize(new Dimension(100, 20));
+		txfTelefone.setPreferredSize(new Dimension(100, 20));
+		GridBagConstraints gbc_txfTelefone = new GridBagConstraints();
+		gbc_txfTelefone.gridwidth = 2;
+		gbc_txfTelefone.insets = new Insets(0, 0, 0, 5);
+		gbc_txfTelefone.anchor = GridBagConstraints.WEST;
+		gbc_txfTelefone.gridx = 1;
+		gbc_txfTelefone.gridy = 2;
+		panel.add(txfTelefone, gbc_txfTelefone);
+		txfTelefone.setColumns(15);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 1;
+		contentPane.add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{0, 0};
+		gbl_panel_1.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		btnNovo = new JButton("Novo");
+		GridBagConstraints gbc_btnNovo = new GridBagConstraints();
+		gbc_btnNovo.anchor = GridBagConstraints.EAST;
+		gbc_btnNovo.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNovo.gridx = 0;
+		gbc_btnNovo.gridy = 0;
+		panel_1.add(btnNovo, gbc_btnNovo);
+		
+		btnSalvar = new JButton("Salvar");
+		GridBagConstraints gbc_btnSalvar = new GridBagConstraints();
+		gbc_btnSalvar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSalvar.gridx = 1;
+		gbc_btnSalvar.gridy = 0;
+		panel_1.add(btnSalvar, gbc_btnSalvar);
+		
+		btnExcluir = new JButton("Excluir");
+		GridBagConstraints gbc_btnExcluir = new GridBagConstraints();
+		gbc_btnExcluir.gridx = 2;
+		gbc_btnExcluir.gridy = 0;
+		panel_1.add(btnExcluir, gbc_btnExcluir);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 2;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
 	}
 
 }
