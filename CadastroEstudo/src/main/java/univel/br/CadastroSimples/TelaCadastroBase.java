044323089add5b6cd913bf9cdb23c8404d262cb3
Andre Tosetto <Andre Tosetto@Andre-PC.home>
2017-06-12 11:29:41 -0300

    Criando Projeto

---
+++ CadastroEstudo/src/main/java/univel/br/CadastroSimples/TelaCadastroBase.java
@@ -0,0 +1,43 @@
+package univel.br.CadastroSimples;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TelaCadastroBase extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCadastroBase frame = new TelaCadastroBase();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaCadastroBase() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		System.out.println("AAAAAAAAA");
+	}
+
+}
